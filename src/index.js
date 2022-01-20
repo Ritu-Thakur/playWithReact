1f3bf53a98ebcb64928c05eadd3c900f20b21d93
Ritu-Thakur <30444945+Ritu-Thakur@users.noreply.github.com>
2022-01-20 10:23:56 -0500

    Changed component name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App1 from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <App1 />
   </StrictMode>,
   rootElement
 );
